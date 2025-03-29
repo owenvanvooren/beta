9f3c2f0d0a66698d149d4c122aacab2eb2e5c0ba
Owen Van Vooren <hi@owen.uno>
2025-03-28 19:15:27 -0500

    AutoUpdate

--- index.js
+++
@@ -1,5 +0,0 @@
-// Entry point for Vercel deployment
-// This file simply requires the main server file
-
-// Import the main server file
-require('./cdn-server.js'); 
--- run-local.js
+++
@@ -1,7 +0,0 @@
-// Script to run the server locally with dotenv
-
-// Load environment variables from .env file
-require('dotenv').config();
-
-// Import the main server file
-require('./cdn-server.js'); 
